6937fa25b373de827214c98b7aceea581d2cdd1c
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-01 13:09:44 +0530

    Solution : append()

--- index.js
+++ index.js
@@ -17,18 +17,27 @@ class LinkList {
   constructor(value) {
     this.head = {
       value: value,
-      next : null
+      next: null,
     };
     this.tail = this.head;
     this.length = 1;
   }
-  append(value){
 
+  append(value) {
+    const newNode = {
+      value: value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length++;
+    return this;
   }
+  
 }
 
 const myLinkedList = new LinkList(10);
-myLinkedList.append(5)
-myLinkedList.append(10)
-myLinkedList.append(15)
+myLinkedList.append(5);
+myLinkedList.append(10);
+myLinkedList.append(15);
 console.log(myLinkedList);

4e7f860642f84d96e255c82de2a51446e3a45864
Gaurav Sutar <gauravstr05@gmail.com>
2022-10-01 12:55:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+// Our First LinkList
+
+// let myLinkedList = {
+//   head: {
+//     value: 10,
+//     next: {
+//       value: 5,
+//       next: {
+//         value: 16,
+//         next: null,
+//       },
+//     },
+//   },
+// };
+
+class LinkList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next : null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value){
+
+  }
+}
+
+const myLinkedList = new LinkList(10);
+myLinkedList.append(5)
+myLinkedList.append(10)
+myLinkedList.append(15)
+console.log(myLinkedList);
